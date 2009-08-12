cf23d2f2cfd6fd18c710741135b99c9f1c0fc32d
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-12 15:31:04 +0000

    try 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bvpsolve@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mebdfi/src/deSolve.h
@@ -0,0 +1,83 @@
+#include <R.h>
+#include <Rdefines.h>
+/* global variables */
+SEXP Time, Y, YPRIME , Rin;
+extern SEXP de_gparms;
+
+typedef void deriv_func(int *, double *, double *,double *,double *, int *);
+deriv_func * derfun;
+
+typedef void res_func(double *, double *, double *, double*, double *,
+                      int*, double *, int*);
+res_func * res_fun;
+
+typedef void init_func (void (*)(int *, double *));
+
+void updatedeforc(double *);
+
+/* livermore solver globals */
+extern SEXP odesolve_deriv_func;
+extern SEXP odesolve_jac_func;
+extern SEXP odesolve_jac_vec;
+extern SEXP odesolve_root_func;
+extern SEXP odesolve_envir;
+
+/* daspk globals */
+extern SEXP daspk_res_func;
+extern SEXP daspk_jac_func;
+extern SEXP daspk_psol_func;
+extern SEXP daspk_envir;
+extern SEXP daspk_res_out;
+
+/* utilities */
+void init_N_Protect(void);
+void incr_N_Protect(void);
+void unprotect_all(void);
+void my_unprotect(int);
+
+
+/* declarations for initialisations;*/
+void initParms(SEXP Initfunc, SEXP Parms);
+void Initdeparms(int *, double *);
+void Initdeforc(int *, double *);
+void initOut(int isDll, int neq, SEXP nOut, SEXP Rpar, SEXP Ipar);
+
+
+void sparsity1D (SEXP Type, int* iwork, int neq, int liw);
+void sparsity2D (SEXP Type, int* iwork, int neq, int liw);
+void sparsity3D (SEXP Type, int* iwork, int neq, int liw);
+
+/* use in daspk */
+int n_eq;
+int mu;
+int ml;
+int nrowpd;
+
+/* output in DLL globals */
+int nout, ntot, isOut, lrpar, lipar, *ipar;
+double *out;
+
+/* KS globals for the forcings */
+int initForcings(SEXP list);
+
+SEXP getListElement(SEXP list, const char *str);
+
+long int nforc;  /* the number of forcings */
+
+/* Input data. three vectors:
+  tmat, fmat: time, forcing function data value
+  imat: index to start of each forcing function in tmat, fmat*/
+double * tvec;
+double * fvec;
+int    * ivec;
+int    fmethod;
+
+/* for each forcing function: index to current position in tmat, fmat,
+ current value, interpolation factor, current forcing time, next forcing time,
+ max time (to be removed).....
+*/
+int    * findex;
+double * intpol;
+int    * maxindex;
+
+double * forcings;
