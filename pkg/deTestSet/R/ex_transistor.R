97ae15ba6bd31382c121de836257e4aa8403f26e
mazzia <mazzia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-28 08:19:57 +0000

    transistor: added mescd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bvpsolve@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/deTestSet/R/ex_transistor.R
+++ pkg/deTestSet/R/ex_transistor.R
@@ -9,7 +9,7 @@
 ## =============================================================================
 
 transistor <- function(times = seq(0, 0.2, 0.001), yini = NULL, dyini = NULL,
-  parms=list(), method = "mebdfi", maxsteps = 1e5, ...) {
+  parms=list(), method = "mebdfi", maxsteps = 1e5, atol = 1e-6, rtol = 1e-6,  printmescd = TRUE, ...) {
 
 ### check input 
     parameter <- c(ub=6, uf=0.026, alpha=0.99, beta=1e-6,
@@ -29,7 +29,8 @@ transistor <- function(times = seq(0, 0.2, 0.001), yini = NULL, dyini = NULL,
            -yini[5]/(c4*r7), -10.00564453, -10.00564453))
 
     checkini(8, yini, dyini)
-    
+	
+	prob <- transistorprob()
 ### solve
 
    ind <- c(8,0,0)  # index of the system
@@ -41,12 +42,12 @@ transistor <- function(times = seq(0, 0.2, 0.001), yini = NULL, dyini = NULL,
       useres <- TRUE
 
     if (useres)
-      return(dae(y = yini, dy = dyini, times = times,
+      out<-dae(y = yini, dy = dyini, times = times,
                   res = "transres", nind = ind,
                   dllname = "deTestSet",  initfunc = "transpar",
                   jactype = "bandint", banddown = 2, bandup = 1,
-                  parms = parameter, maxsteps = maxsteps, ...) )
-
+                  parms = parameter, maxsteps = maxsteps, atol=atol, rtol=rtol, ...) 
+    else { 
      mass <- matrix(nrow = 3, ncol = 8, data=0)
      mass[1,2] <- parameter["c1"]
      mass[1,5] <- parameter["c3"]
@@ -66,7 +67,44 @@ transistor <- function(times = seq(0, 0.2, 0.001), yini = NULL, dyini = NULL,
                 banddown = 2, bandup = 1,
                 dllname = "deTestSet", initfunc = "transpar",
                 parms = parameter,
-                method = method, maxsteps = maxsteps, ...)
+                method = method, maxsteps = maxsteps,atol=atol, rtol=rtol, ...)
+	}
+		if (printmescd & ( out[nrow(out),1] == prob$t[2] )) { 
+			ref = reference("transistor")
+			mescd = min(-log10(abs(out[nrow(out),-1] - ref)/(atol/rtol+abs(ref))))
+			printM(prob$fullnm)
+			cat('Solved with ')
+			printM(attributes(out)$type)
+			cat('Using rtol = ')
+			cat(rtol)
+			cat(', atol=')
+			printM(atol)
+			printM("Mixed error significant digits:")
+			printM(mescd)}
+		
+		
   return(out)
 }
+
+
+
+
+
+transistorprob <- function(){
+	fullnm <- "Transistor Amplifier"
+	problm <- 'transamp'
+	type <- 'DAE'
+	neqn <- 8
+	ndisc <- 0
+	t <- matrix(1,2)
+	t[1] <- 0
+	t[2] <- 0.2
+	numjac <- FALSE
+	mljac <- 2
+	mujac <- 1
+	
+	return(list(fullnm=fullnm, problm=problm,type=type,neqn=neqn,
+					t=t,numjac=numjac,mljac=mljac,mujac=mujac))
+}
+
 
