ccd994369979e0d269185b80ee8ab20125523f32
mazzia <mazzia@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-12-12 16:06:16 +0000

    possible uninitialized variables
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bvpsolve@256 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/deTestSet/src/bimd.f
+++ pkg/deTestSet/src/bimd.f
@@ -1441,6 +1441,9 @@ C
 
 C-------------------------------------------------------------------------------------------------------------------
 C     OTHER INITIALIZATIONS
+CF
+      qinfj = .FALSE.
+      qinff = .FALSE.
 
       IF (INDEX1.EQ.M) THEN
          INDEXD = 1
@@ -1482,6 +1485,53 @@ C     VECTOR TO BE USED FOR THE ESTIMATE OF JACOBIAN VARIATION
       ORD_IND = INT(ORD/2) - 1
       K       = STEP_ORD(ORD_IND)
       KOLD    = 0
+CF Initialize the parameters
+      GAMMA  = 0.0d0
+      RHOT   = 0.0d0
+      RHOTUP = 0.0d0
+      RHOI   = 0.0d0
+      RHOIUP = 0.0d0
+      RHOOLD = 0.0d0
+      RHOMU  = 0.0d0
+      RHOML  = 0.0d0
+      RHOJ0  = 0.0d0
+      RHOFATT = 0.0d0
+      SISERR = 0
+      SISERRUP  = 0
+      FATERR = 0.0d0
+      VMAX(1)  = 0.0d0
+      VMAX(2)  = 0.0d0
+      VMAX(3)  = 0.0d0
+      VMAX(4)  = 0.0d0
+      VMAX(5)  = 0.0d0
+      VMAX(6)  = 0.0d0
+      VMAX(7)  = 0.0d0
+      VMAX(8)  = 0.0d0
+      FATDJ0 = 0.0d0
+      FATDJ0I = 0.0d0
+      tolrhoJ0 = 0.0d0
+      DELTAH2= 0.0d0
+      DELTAH1SF = 0.0d0
+      CFAT1  = 0.0d0
+      CFAT2  = 0.0d0
+      cscal = 0.0d0
+      NORD     = 0
+      NJ00 = 0
+      NERROR = 0
+      MINIT = 0
+      MAXIT = 0
+      MAXf0 = 0
+      it0 = 0
+      it = 0
+      hj0 = 0
+      fminy0 = 0.0d0
+      extraps = .false.
+      espup = 0.0d0
+      espdn = 0.0d0
+      esp = 0.0d0
+      error = .false.
+      mudif = 0
+CF
       H       = DMIN1(H,HMAX)
 C -------------------------------------------------------
 C     INITIAL STEPSIZE TOO SMALL !!!!!
